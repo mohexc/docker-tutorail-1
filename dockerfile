90ec910f1e5d0f24c7bfc4be4ee920124d1631ed
nanut <galunyu@gmail.com>
2020-11-09 01:26:46 +0700

    [Start] '

---
+++ dockerfile
@@ -0,0 +1,11 @@
+FROM node:10
+
+RUN mkdir -p /usr/src/app
+
+COPY / /usr/src/app/
+
+WORKDIR /usr/src/app
+
+CMD ['node', "/usr/src/app/server.js"]
+
+EXPOSE 5000    
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hello word!')
+})
+
+app.listen(3000, () => {
+    console.log(`example app listening on port 3000`)
+})
